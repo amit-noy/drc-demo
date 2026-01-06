54d11566590ba711cb9e9ae3652dfb41dcffd9d0
amit-noy <Amit_Noyberg@external.mckinsey.com>
2026-01-06 12:34:02 +0200

    update button primary color (#11)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,7 +16,7 @@ export const Button: FC<ButtonProps> = ({
                 borderRadius: 6,
                 border: '1px solid #ccc',
                 cursor: 'pointer',
-                backgroundColor: variant === 'primary' ? '#ff0026' : '#fff'
+                backgroundColor: variant === 'primary' ? '#0015ff' : '#fff'
             }}
         >
             {children}
